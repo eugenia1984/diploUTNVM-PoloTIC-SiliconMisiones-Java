e085352b71ca7d4037e4747aeef3f6881860cbb0
Eugenia <costamariaeugenia1@gmail.com>
2021-08-29 15:10:07 -0300

    subo ejercicio 1 de practica

---
+++ polotic_siliconmisiones/clase03_bucles_while_for/Clase3Ejercicio1/src/clase3ejercicio1/Clase3Ejercicio1.java
@@ -0,0 +1,17 @@
+/*
+ * Realizar un programa que muestre en pantalla los números del 1 al 35
+ * (uno abajo del otro).
+ * Utilizar para esto alguna estructura repetitiva.
+ */
+package clase3ejercicio1;
+
+public class Clase3Ejercicio1 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 36; i++) { 
+    //inicio en 1, hasta llegar a 35 (<36) y voy sumando 1 en 1
+            System.out.println(i);
+        }
+    }
+    
+}
