2bb593e53a752f35116566dd730c2d009cfd8745
Eugenia <costamariaeugenia1@gmail.com>
2021-08-29 15:27:46 -0300

    ejercicio 4

---
+++ polotic_siliconmisiones/clase03_bucles_while_for/Clase3Ejercicio4/src/clase3ejercicio4/Clase3Ejericicio4.java
@@ -0,0 +1,16 @@
+/*
+ * Realizar un programa que lleve a cabo la cuenta regresiva para el año nuevo, 
+ * la cuenta debe comenzar en 10 y terminar en 1.
+ */
+package clase3ejercicio4;
+
+public class Clase3Ejericicio4 {
+
+    public static void main(String[] args) {
+        System.out.println("--- Cuenta regresiva del 10 al 1 ---");
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+ 
+}
